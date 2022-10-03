4bdd359d09b9d7748f3d190ac762706adb109984
Anwehan Roy Chowdhury <anweshanrc15@gmail.com>
2022-10-04 04:38:45 +0530

    :bug: Fixed check content warning

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,jsx,tsx,ts}',
+    './index.html',
+    'public/404.html'
   ],
   theme: {
     extend: {},
