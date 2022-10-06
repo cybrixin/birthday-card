5f6ae74d3b9bd73ccc0f1c3b08e215cf10780010
Anwehan Roy Chowdhury <anweshanrc15@gmail.com>
2022-10-06 15:03:15 +0530

    :tada: Fixing & adding components
    :yawning_face: Most components are till now lazy loaded;
    Code splitting is also taken care due to lazy load.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,27 +1,9 @@
-import { useApp } from "@/contexts/AppContext"
-import { useRef, useEffect } from "react";
-import { getUrl } from "@/util/util";
+import React from "react"
+
+const Audio = React.lazy( () => import('@/components/Audio'));
 
 export default function App() : JSX.Element {
-  const { storage } = useApp();
-  const inputElement = useRef<any>(undefined);
-
-  useEffect(() => {
-      if(storage != null) {
-        console.log("Hold My Hand 🤝🏻");
-        const { bucket } = inputElement.current.dataset;
-        
-        getUrl({ storage, bucket, callback: (url: string) => {
-              inputElement.current.src = url;
-            }
-          });
-      }
-  }, [storage]);
-  
   return (
-    <>
-      There should hear a song 🎶 below 👇🏻:
-      <audio ref={inputElement} muted={true} controls={true} data-bucket="birthday/audio/HappyBirthday.mp3"></audio>
-    </>
+      <Audio />
   )
 }
