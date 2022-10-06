70cf5e14292f4fd99b1295c1dc10c9c3628d07bd
Anwehan Roy Chowdhury <anweshanrc15@gmail.com>
2022-10-07 02:43:00 +0530

    :musical_note: Added audio context
    Now you can interact with the audio button and play the song.

--- src/components/Audio/Audio.tsx
+++ src/components/Audio/Audio.tsx
@@ -10,11 +10,27 @@ const art_sizes = [192, 256, 384, 512]
 
 const artwork: any = [];
 
+const PUBLIC_AUDIO_SKIP_TIME = 10;
+
 let mediaMetadata = {
     title: "Happy Birthday 🎶",
     artist: "Anweshan Roy Chowdhury",
     album: "B'Day Album 💿",
-    artwork: null,
+    artwork: [],
+}
+
+function updatePosition(element: HTMLAudioElement, position: number) {
+    if(element) element.currentTime = position;
+
+    const { duration, playbackRate, currentTime } = element;
+    
+    if('setPositionState' in navigator.mediaSession){
+        navigator.mediaSession.setPositionState({
+            duration,
+            playbackRate,
+            position: currentTime
+        })
+    }
 }
 
 export default function Audio() {
@@ -24,12 +40,22 @@ export default function Audio() {
     const [ playing, setPlaying ] = useState(false);
 
     const [ audioUrl, setAudioUrl ] = useState<string | null>(null);
+
+    const [audioElement, setAudioElement] = useState<HTMLAudioElement | undefined>(undefined);
     
     const [ loading, setLoading ] = useState(true);
 
     const toggleMusic = (event: any) => {
         event.preventDefault();
-        setPlaying(!playing);
+        if(audioElement?.paused){
+            audioElement?.play()?.then(_ => {
+                updatePosition(audioElement, audioElement.currentTime);
+                setPlaying(true);
+            });
+        }else {
+            audioElement?.pause()
+            setPlaying(false);
+        }
     };
 
     useEffect( () => {
@@ -65,6 +91,47 @@ export default function Audio() {
 
     useEffect( () => {
         if(audioUrl != null) {
+            if('mediaSession' in navigator) {
+                console.log(mediaMetadata);
+                navigator.mediaSession.metadata = new MediaMetadata(mediaMetadata);
+            }
+
+            const audio = document.createElement("audio");
+            audio.src = audioUrl;
+
+            if('mediaSession' in navigator &&'setActionHandler' in navigator.mediaSession){
+
+                navigator.mediaSession.setActionHandler('play', async () => await audio.play());
+                
+                navigator.mediaSession.setActionHandler('pause', () => audio.pause());
+
+                navigator.mediaSession.setActionHandler('seekbackward', (evt) => {
+                    const skipTime = evt.seekOffset || PUBLIC_AUDIO_SKIP_TIME;
+
+                    const position = Math.max(audio.currentTime - skipTime, 0);
+
+                    updatePosition(audio, position);
+                })
+
+                navigator.mediaSession.setActionHandler('seekforward', (evt) => {
+                    const skipTime = evt.seekOffset || PUBLIC_AUDIO_SKIP_TIME;
+                    const position = Math.max(audio.currentTime + skipTime, 0);
+
+                    updatePosition(audio, position);
+                })
+
+            }
+
+            audio.onplay = () => {
+                setPlaying(true);
+            }
+
+            audio.onpause = () => {
+                setPlaying(false);
+                updatePosition(audio, 0);
+            }
+
+            setAudioElement(audio);
             setPlaying(false);
         }
     }, [audioUrl]);
