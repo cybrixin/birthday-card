9946155cee7ce1ac26feddabf23c2151bef72104
Anwehan Roy Chowdhury <anweshanrc15@gmail.com>
2022-10-07 02:46:48 +0530

    :rocket: Added multiple elements
    :card_index: Added the card container
    
    :loud_sound: Added the Audio Container
    
    :pager: Added the toast-container to enable notifying via toast messages

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,16 @@
 import React from "react"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Audio = React.lazy( () => import('@/components/Audio'));
+const Card = React.lazy( () => import("@/components/Card"));
 
 export default function App() : JSX.Element {
   return (
-      <Audio />
+      <>
+        <Card greet="" />
+        <Audio />
+        <ToastContainer />
+      </>
   )
 }
