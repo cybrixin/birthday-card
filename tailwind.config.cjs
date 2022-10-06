ba26583bdf5dc003e424de58dd4902189e9b93b3
Anwehan Roy Chowdhury <anweshanrc15@gmail.com>
2022-10-06 17:21:18 +0530

    :art: Added color palette to tailwindcss configuration.
    Multiple shades of the color blackpink (Custom Name) :wink: has been added!

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,22 @@ module.exports = {
     'public/404.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blackpink: {
+          50: '#c75acc',
+          500: '#8b0077',
+          400: '#9612eb',
+          600: '#8b31d6',
+          700: '#6a24a6',
+          800: '#8b31d6',
+        },
+        lightred: {
+          50: '#ddd6f3',
+          100: '#faaca8',
+        }
+      }
+    },
   },
   plugins: [],
 }
