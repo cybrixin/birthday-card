eb3fbd7a270d6f283fd4d9edbd280833b435e640
Anwehan Roy Chowdhury <anweshanrc15@gmail.com>
2022-10-16 05:50:37 +0530

    :eye: Changed

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,14 +1,20 @@
 import { useApp } from '@/contexts/AppContext'
-import React, { useEffect, useState } from 'react';
+import { lazy, useEffect, useState } from 'react';
 import { getUrl, sleep } from "@/util/util";
 import Spinner from "@/components/Spinner";
 import styles from './Card.module.css';
+import useResize from '@/hooks/useResize';
+
+const Unload = lazy(() => import("@/components/Unload"))
+
 
 const bgStyle = `linear-gradient(to bottom, rgba(255, 255, 255), rgba(255, 255, 255, 0.5))`;
 
 const cakeBucket = 'birthday/images/cake.jpg';
 
 const coverBucket = 'birthday/images/bd.png';
+
+const heightBreakPoint = 481;
 
 export default function Card( { greet='' } : CardProps ) {
     
@@ -18,8 +24,11 @@ export default function Card( { greet='' } : CardProps ) {
 
     const { storage } = useApp();
     const [ loading, setLoading ] = useState<boolean>(true);
-    const [ bgUrl, setBgUrl ] = useState<string | null>(null);
+    const [ isTriggered, setTriggered ] = useState<boolean>(false);
+    const [ bgUrl, setBgUrl ] = useState<string>('');
     const [ coverUrl, setCoverUrl ] = useState<string>('');
+
+    const [ dimensions ] = useResize();
 
     useEffect( () => {
         if(storage != null) {
@@ -35,51 +44,76 @@ export default function Card( { greet='' } : CardProps ) {
 
 
     useEffect( () => {
-        if(bgUrl != null && coverUrl.length > 0 && loading == true) {
+        if(bgUrl.length > 0 && coverUrl.length > 0 && loading == true) {
             setLoading(!loading);
         }
     }, [bgUrl, coverUrl]);
 
 
+    const trigger = !loading && !isTriggered && dimensions.height > heightBreakPoint;
+
+    const isBreakpoint = dimensions.height > heightBreakPoint ; 
+
     useEffect( () => {
-        if(!loading) {
+        // console.log('triggered');
+        if( trigger ) {
+            // console.log('triggered useEffect');
             const notify = async () => {
                 
                 const { toast } = await import('react-toastify');
 
                 toast('Click on the card to see the message ✉.', {
                     position: "top-left",
+                    toastId: "clickCard",
                     autoClose: 8000,
                     hideProgressBar: false,
                     closeOnClick: true,
                     pauseOnHover: true,
                     draggable: true,
-                    progress: undefined,
                 });
 
                 await sleep(5000);
 
                 toast('Click on the ▶ below to hear your birthday song 🎶.', {
                     position: "top-left",
+                    toastId: "clickPlay",
                     autoClose: 8000,
                     hideProgressBar: false,
                     closeOnClick: true,
                     pauseOnHover: true,
                     draggable: true,
-                    progress: undefined,
                 });
             };
-
+            setTriggered(true);
             sleep(2000).then(_ => notify());
         }
-    }, [loading]);
+    }, [trigger]);
+
+
+    useEffect(() => {
+        if(!isBreakpoint && isTriggered) {
+            (async () => {
+                const { toast } = await import('react-toastify');
+                toast.dismiss();
+                setTriggered(false);
+            })();
+        }
+    }, [isBreakpoint])
     
     
     return (
-       ( 
-            loading ? <Spinner /> : 
-            
-            (<div className={styles["card-bd"]} style={{backgroundImage: `${bgStyle}, url('${bgUrl}')`}}>
+        loading ? <Spinner /> : ( isBreakpoint ? <CardComponent bgUrl={bgUrl} coverUrl={coverUrl} greet={greet}/> : <Unload /> )
+    )
+}
+
+type CardComponentProps = {
+    bgUrl: string;
+    coverUrl: string;
+} & CardProps;
+
+const CardComponent = ({ bgUrl, coverUrl, greet} : CardComponentProps ) : JSX.Element => {
+  return (
+    <div className={styles["card-bd"]} style={{backgroundImage: `${bgStyle}, url('${bgUrl}')`}}>
             <div className={styles["back"]}></div>
             <div className={styles["front"]}>
                 <div className={styles["imgset"]}>
@@ -93,7 +127,7 @@ export default function Card( { greet='' } : CardProps ) {
                 <p>May God bless You 🙏🏻</p>
                 <p className={styles["foot"]}>Anweshan Roy Chowdhury</p>
             </div>
-        </div>)
-       )
-    )
+        </div>
+  )
 }
+
