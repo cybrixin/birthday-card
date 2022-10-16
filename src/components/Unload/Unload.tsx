a6a2b24775304dee7f4c5c86ffbd03f6c5bfbdb3
Anwehan Roy Chowdhury <anweshanrc15@gmail.com>
2022-10-17 05:00:55 +0530

    :sparkles: Modified the no-render component
    Added suggestions based on touch & rotate detection.

--- src/components/Unload/Unload.tsx
+++ src/components/Unload/Unload.tsx
@@ -1,10 +1,21 @@
+import useResize from '@/hooks/useResize';
 import styles from './Unload.module.css';
 
+const heightBreakPoint: number = 481;
+
 export default function Unload() : JSX.Element {
+
+    const [ dimensions ] = useResize();
+
+    const { width, height } = dimensions;
+
+    const touchPoints = ( height > heightBreakPoint || width > heightBreakPoint ) && !!navigator.maxTouchPoints;
+
     return (
       <div className={styles.container}>
           <div className={styles['text-area']}>
-                <p>😞 The device is too small to display the card properly.</p>
+                <p className={styles.p1}>😞 The device is too small to display the card properly.</p>
+                { touchPoints ? <p className={styles.p2}>You could rotate <span className={styles.emoji}>🔄</span> your device to view the same.</p> : ""} 
           </div>
       </div>
     )
