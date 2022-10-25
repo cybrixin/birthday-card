b519ba69f75a2dbac4fac7cac3ae44f27c17aa79
Anweshan Roy Chowdhury <anweshanrc15@gmail.com>
2022-10-25 05:57:20 +0530

    :bug: Fixed some bugs.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,16 +1,35 @@
-import React from "react"
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { lazy } from "react"
+import { ToastContainer } from 'react-toastify';
+import AppContext from "@/contexts/AppContext";
 
-const Audio = React.lazy( () => import('@/components/Audio'));
-const Card = React.lazy( () => import("@/components/Card"));
+const Audio = lazy( () => import('@/components/Audio'));
+const Card = lazy( () => import("@/components/Card"));
 
 export default function App() : JSX.Element {
+  const res = [{
+    parent: document.head,
+    tag: 'link',
+    attr: {
+      href: 'https://fonts.googleapis.com/css2?family=Cedarville+Cursive&family=Kalam:wght@400;700&family=Sacramento&display=swap',
+      rel: 'stylesheet',
+      type: 'text/css',
+    }
+  },{
+    parent: document.head,
+    tag: 'link',
+    attr: {
+      href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
+      rel: 'stylesheet',
+      type: 'text/css',
+    }
+  }];
+
   return (
-      <>
-        <Card greet="" />
-        <Audio />
-        <ToastContainer />
-      </>
-  )
+    <AppContext resource={res}>
+      <Card greet="" />
+      <Audio />
+      <ToastContainer />
+    </AppContext>
+  );
 }
